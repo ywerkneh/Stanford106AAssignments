c6d28bb3820fa51baa0105e5334a59fa21889bc8
Yehualashet Werkneh <fasil.son@gmail.com>
2021-03-06 17:52:03 -0500

    week3 commits

--- Eclipse/Stanford106AAssignments/src/week3/AddCommasToNumberStrings.java
+++ Eclipse/Stanford106AAssignments/src/week3/AddCommasToNumberStrings.java
@@ -3,9 +3,24 @@ package week3;
 import acm.program.ConsoleProgram;
 
 public class AddCommasToNumberStrings extends ConsoleProgram {
-	
-	public void run() {
 
+	public void run() {
+		System.out.println(addCommasToNumericString("1000"));
 	}
-	
+
+	private String addCommasToNumericString(String digits) {
+		String result = "";
+		int len = digits.length();
+		int nDigits = 0;
+		for (int i = len - 1; i >= 0; i--) {
+		result = digits.charAt(i) + result;
+		nDigits++;
+		// we also need i > 0 so we don’t have a comma before the
+		if (((nDigits % 3) == 0) && (i > 0)) {
+		result = "," + result;
+		}
+		}
+		return result;
+		
+		}
 }
--- Eclipse/Stanford106AAssignments/src/week3/DeleteCharactersFromString.java
+++ Eclipse/Stanford106AAssignments/src/week3/DeleteCharactersFromString.java
@@ -5,7 +5,20 @@ import acm.program.ConsoleProgram;
 public class DeleteCharactersFromString extends ConsoleProgram {
 	
 	public void run() {
+		System.out.println(" All occurences of 'a' removed from the string asdasdjaksdkasdasda gives : " 
+							+ removeAllOccurrences("\n asdasdjaksdkasdasda", 'a'));
+		
 
 	}
 	
-}
+		private String removeAllOccurrences(String str, char ch) {
+			String result = "";
+			for (int i = 0; i < str.length(); i++) {
+			if (str.charAt(i) != ch) {
+			result += str.charAt(i);
+			}
+			}
+			return result;
+			}
+	}
+
